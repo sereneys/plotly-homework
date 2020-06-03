5e50db1cb3d728a13123121dfd7734c42f20314c
sereneys <sereneyangshu@gmail.com>
2020-06-02 22:50:56 -0700

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 function buildChart(sampleName) {
     var selector = d3.select("#sample-metadata")
 
-    d3.json("samples.json").then((data) => {
+    d3.json("https://sereneys.github.io/plotly-homework/data/samples.json").then((data) => {
 
         var samples = data.samples;
         var filterResult = samples.filter(sample => sample.id == sampleName);
@@ -17,7 +17,7 @@ function buildChart(sampleName) {
         var barData = [
             {
                 y: yticks,
-                x: sample_valuess.slice(0,10).reverse(),
+                x: sample_values.slice(0,10).reverse(),
                 text: otu_labels.slice(0,10).reverse(),
                 type: "bar",
                 orientation: "h",
@@ -31,14 +31,36 @@ function buildChart(sampleName) {
 
         Plotly.newPlot("bar", barData, barLayout)
 
+        //Create bubble chart
 
+        var bubbleData = [
+            {
+                x: otu_ids,
+                y: sample_values,
+                text: otu_labels,
+                mode: "markers",
+                marker: {
+                    size: sample_values,
+                    color: otu_ids,
+                    colorscale: "Earth",
+                }
+            }
+        ];
+
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Samples",
+            margin:{ t: 30},
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
     });
-
 }
 
 function buildMetaData(sampleName) {
 
-    d3.json("samples.json").then((data) => {
+    d3.json("https://sereneys.github.io/plotly-homework/data/samples.json").then((data) => {
 
         var samples = data.metadata;
         var filterResult = samples.filter(sample => sample.id == sampleName);
@@ -48,38 +70,35 @@ function buildMetaData(sampleName) {
 
         selector.html("");
 
-        Object.defineProperties(result).forEach(([key, value]) => {
+        Object.entries(result).forEach(([key, value]) => {
             selector.append("h6").text(`${key.toUpperCase()}: ${value}`);
         });
-
     });
-
 }
 
-d3.selectAll("#selDataset").on("change", optionChange());
+d3.selectAll("#selDataset").on("change", optionChanged());
 
 function init() {
 
     var selector = d3.select("#selDataset");
 
-    d3.json("samples.json").then((data) => {
+    d3.json("https://sereneys.github.io/plotly-homework/data/samples.json").then((data) => {
 
         var names = data.names;
 
         names.forEach((sample) => {
             selector.append("option").property("value", sample).text(sample); 
+
+        var defaultName = names[0];
+        buildChart(defaultName);
+        buildMetaData(defaultName);
         });
     });
-
-    var defaultName = names[0];
-    buildChart(defaultName);
-    buildMetaDate(defaultName);
-
 }
 
-function optionChange(newSample) {
+function optionChanged(newSample) {
     buildChart(newSample);
-    buildMetaDate(newSample);
+    buildMetaData(newSample);
 }
 
 init();
